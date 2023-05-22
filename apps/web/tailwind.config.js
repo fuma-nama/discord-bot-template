35901ef1a67981c824803b5af85e75246736e093
SonMooSans <god63820869@gmail.com>
2023-05-22 21:49:38 +0800

    Adjust container width

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
             center: true,
             padding: "2rem",
             screens: {
-                "2xl": "1400px",
+                "2xl": "1200px",
             },
         },
         extend: {
