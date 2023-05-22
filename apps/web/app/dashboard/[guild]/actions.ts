02cd42e1faf6c70ee4418928da92d84a5d7e06e4
SonMooSans <god63820869@gmail.com>
2023-05-22 21:18:13 +0800

    Remove unused files

--- apps/web/app/dashboard/[guild]/actions.ts
+++
@@ -1,6 +0,0 @@
-"use server";
-import { revalidatePath } from "next/cache";
-
-export async function refreshGuild(guild: string) {
-    revalidatePath(`https://discord.com/api/v9/guilds/${guild}`);
-}
