68ccee564fd2565cd518fca34c211a619e31d4df
SonMooSans <god63820869@gmail.com>
2023-05-23 22:16:25 +0800

    Fix type errors

--- apps/web/app/form.tsx
+++ apps/web/app/form.tsx
@@ -9,7 +9,7 @@ export function Form() {
     return (
         <button
             className="bg-white text-black px-4 py-2 rounded-md"
-            onClick={trigger}
+            onClick={() => trigger()}
         >
             {isMutating ? "Running" : "Run Action"}
         </button>
