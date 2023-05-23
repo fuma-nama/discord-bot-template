932345ead71ef6e75146b3fb7dd44e6f33ac9eea
SonMooSans <god63820869@gmail.com>
2023-05-23 22:05:23 +0800

    Split server actions

---
+++ apps/web/app/dashboard/[guild]/(features)/features/welcome-message/actions.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { prisma } from "@/utils/prisma";
+import { z } from "zod";
+
+const schema = z.object({
+    channel: z.string().nullable(),
+    message: z.string(),
+});
+
+export type Data = z.infer<typeof schema>;
+
+export async function disable(guild: string) {
+    "use server";
+
+    await prisma.welcomeFeature.deleteMany({
+        where: {
+            guild_id: guild,
+        },
+    });
+    revalidatePath(`/dashboard/${guild}/features/welcome-message`);
+}
+
+export async function save(guild: string, raw: Data) {
+    "use server";
+    const data = schema.parse(raw);
+
+    await prisma.welcomeFeature.updateMany({
+        data: {
+            message: data.message,
+            channel_id: data.channel,
+        },
+        where: {
+            guild_id: guild,
+        },
+    });
+
+    revalidatePath(`/dashboard/${guild}/features/welcome-message`);
+}
