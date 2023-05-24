12996471fb7acc77743d3f8073f3fb8e2d2346b0
SonMooSans <god63820869@gmail.com>
2023-05-24 21:43:44 +0800

    Improve home page

---
+++ apps/web/app/(info)/page.tsx
@@ -0,0 +1,98 @@
+import Link from "next/link";
+import { cn } from "@/utils/cn";
+import { buttonVariants } from "@/components/ui/button";
+import clsx from "clsx";
+import { invite_url } from "@/utils/shared";
+
+export default async function Home() {
+    return (
+        <main className="flex flex-1 flex-col items-center gap-24 p-24 container">
+            <div
+                className={clsx(
+                    "z-[2] mt-auto relative flex flex-col gap-2 place-items-center",
+                    "before:-z-[1] before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] before:lg:h-[360px] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10",
+                    "after:-z-[1] after:absolute after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40"
+                )}
+            >
+                <h1 className="text-4xl font-bold dark:drop-shadow-[0_0_0.3rem_#ffffff70]">
+                    Discord Bot
+                </h1>
+                <p className="text-muted-foreground">
+                    The best Discord bot you've ever seen
+                </p>
+                <div className="mt-4 flex flex-row gap-4">
+                    <Link href="/dashboard" className={cn(buttonVariants())}>
+                        Dashboard
+                    </Link>
+                    <a
+                        href={invite_url}
+                        target="_blank"
+                        rel="noopener noreferrerr"
+                        className={cn(buttonVariants({ variant: "secondary" }))}
+                    >
+                        {`Invite ->`}
+                    </a>
+                </div>
+            </div>
+
+            <div className="mt-auto grid text-center lg:grid-cols-3 lg:text-left">
+                <Link
+                    href="/dashboard"
+                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                >
+                    <h2 className={`mb-3 text-2xl font-semibold`}>
+                        Dashboard{" "}
+                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                            -&gt;
+                        </span>
+                    </h2>
+                    <p
+                        className={`m-0 max-w-[30ch] text-sm text-muted-foreground`}
+                    >
+                        Configure discord bot with the beautiful, user-friendly
+                        dashboard.
+                    </p>
+                </Link>
+
+                <a
+                    href={invite_url}
+                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2 className={`mb-3 text-2xl font-semibold`}>
+                        Invite{" "}
+                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                            -&gt;
+                        </span>
+                    </h2>
+                    <p
+                        className={`m-0 max-w-[30ch] text-sm text-muted-foreground`}
+                    >
+                        Invite and explore powerful features of discord bot.
+                    </p>
+                </a>
+
+                <a
+                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h2 className={`mb-3 text-2xl font-semibold`}>
+                        Deploy{" "}
+                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                            -&gt;
+                        </span>
+                    </h2>
+                    <p
+                        className={`m-0 max-w-[30ch] text-sm text-muted-foreground`}
+                    >
+                        Instantly deploy your Next.js site to a shareable URL
+                        with Vercel.
+                    </p>
+                </a>
+            </div>
+        </main>
+    );
+}
