a6ed04fdfb60af71a9ee55c7c0b53fad59f15858
SonMooSans <god63820869@gmail.com>
2023-05-25 22:47:40 +0800

    Discord AI: Create banner

--- apps/web/app/dashboard/[guild]/(features)/features/ai/page.tsx
+++ apps/web/app/dashboard/[guild]/(features)/features/ai/page.tsx
@@ -1,3 +1,16 @@
+import { Button } from "@/components/ui/button";
+
 export default function AIPage() {
-    return <div></div>;
+    return (
+        <div>
+            <div className="flex flex-col rounded-lg p-4 bg-gradient-to-br from-blue-100 via-pink-200 to-purple-400 text-black shadow-xl shadow-purple-200/20">
+                <h3 className="text-xl font-bold">Discord AI</h3>
+                <p className="text-sm">
+                    Use our powerful AI to generate perfect ideas for you daily
+                    life
+                </p>
+                <Button className="mt-8">Join Waitlist</Button>
+            </div>
+        </div>
+    );
 }
