b98dc99d930bb7de8318281257a1e9b040e01906
SonMooSans <god63820869@gmail.com>
2023-05-27 20:57:54 +0800

    Improve guild overview page

--- apps/web/app/dashboard/[guild]/(dashboard)/page.tsx
+++ apps/web/app/dashboard/[guild]/(dashboard)/page.tsx
@@ -1,5 +1,7 @@
+import { buttonVariants } from "@/components/ui/button";
 import { prisma } from "@/utils/prisma";
 import { DatabaseIcon, TimerIcon, HardDriveIcon } from "lucide-react";
+import Link from "next/link";
 import { ReactNode } from "react";
 const formatter = Intl.NumberFormat();
 
@@ -21,23 +23,54 @@ export default async function GuildPage({
         <>
             <div className="grid gird-cols-1 md:grid-cols-3 gap-4">
                 <Card
-                    icon={<DatabaseIcon className="w-10 h-10 text-dominant" />}
+                    icon={<DatabaseIcon className="w-10 h-10" />}
                     title="Rows Created"
                     body={formatter.format(count)}
                 />
                 <Card
-                    icon={<TimerIcon className="w-10 h-10 text-dominant" />}
+                    icon={<TimerIcon className="w-10 h-10" />}
                     title="Last Record"
                     body={last?.value ?? "No Records"}
                 />
                 <Card
-                    icon={<HardDriveIcon className="w-10 h-10 text-dominant" />}
+                    icon={<HardDriveIcon className="w-10 h-10" />}
                     title="Storage"
                     body={`${count}/1000`}
                 />
-            </div>
-            <div className="grid grid-cols-3">
-                <div className="col-span-2 h-40"></div>
+                <div className="rounded-xl p-4 md:p-8 max-md:row-start-1 md:col-span-2 bg-gradient-to-br from-purple-200 to-cyan-300 text-slate-900 border-[1px]">
+                    <h3 className="text-xl font-semibold">
+                        The Best Discord AI.
+                    </h3>
+                    <p className="text-slate-800 text-sm">
+                        The Generative AI powered by OpenAI
+                    </p>
+                    <div className="mt-auto pt-4 md:pt-6">
+                        <Link
+                            href={`/dashboard/${params.guild}/features/ai`}
+                            className={buttonVariants({
+                                variant: "secondary",
+                            })}
+                        >
+                            Try Now
+                        </Link>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-2 p-4 border-[1px] rounded-lg">
+                    <h3 className="text-lg font-semibold">Tasks</h3>
+
+                    <ul className="mt-auto divide-y-[1px] divide-border">
+                        {["Buy a coffee", "Coding", "Daily Meeting"].map(
+                            (item, i) => (
+                                <li
+                                    key={i}
+                                    className="text-muted-foreground text-sm py-2 last:pb-0"
+                                >
+                                    {item}
+                                </li>
+                            )
+                        )}
+                    </ul>
+                </div>
             </div>
         </>
     );
--- apps/web/app/dashboard/[guild]/features/analytics/page.tsx
+++ apps/web/app/dashboard/[guild]/features/analytics/page.tsx
@@ -10,7 +10,7 @@ export default function Analystics() {
             <div className="flex flex-col p-4 border-[1px] rounded-lg mt-4">
                 <p className="text-muted-foreground text-sm">Last week</p>
                 <h3 className="font-semibold">Command Usage</h3>
-                <div className="pr-4 pb-4 mt-8">
+                <div className="pr-4 pb-4 mt-8 max-w-xl">
                     <Chart />
                 </div>
             </div>
