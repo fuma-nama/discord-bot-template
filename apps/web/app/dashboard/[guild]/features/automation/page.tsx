720e737fe65d93346f0212c071167104a5e920d5
SonMooSans <god63820869@gmail.com>
2023-05-27 20:26:17 +0800

    Implement Automation Page template

---
+++ apps/web/app/dashboard/[guild]/features/automation/page.tsx
@@ -0,0 +1,10 @@
+export default function AutomationPage() {
+    return (
+        <>
+            <h2 className="text-2xl font-bold">Automation</h2>
+            <p className="text-muted-foreground text-sm">
+                Run a repeated process or operation
+            </p>
+        </>
+    );
+}
