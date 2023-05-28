04a1fd473295ca9bf9f637d89b56c31190a2179b
SonMooSans <god63820869@gmail.com>
2023-05-28 17:53:08 +0800

    Fix build errors

--- apps/web/utils/auth/logout.tsx
+++ apps/web/utils/auth/logout.tsx
@@ -2,10 +2,9 @@
 
 import { token_cookie } from "@/utils/shared";
 import { cookies } from "next/dist/client/components/headers";
-import { RequestCookies } from "next/dist/server/web/spec-extension/cookies";
 import { redirect } from "next/navigation";
 
 export async function logoutAction() {
-    (cookies() as RequestCookies).delete(token_cookie);
+    cookies().delete(token_cookie);
     redirect("/");
 }
