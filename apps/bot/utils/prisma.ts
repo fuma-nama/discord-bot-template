93100a79aae0479bf037aa568c99064578985729
SonMooSans <god63820869@gmail.com>
2023-05-29 19:30:34 +0800

    Fix: Cache prisma instance in development mode

--- apps/bot/utils/prisma.ts
+++ apps/bot/utils/prisma.ts
@@ -1,3 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+declare global {
+    var prisma: PrismaClient;
+}
+
+const createClient = () => {
+    if (process.env.NODE_ENV === "development" && globalThis.prisma != null) {
+        return globalThis.prisma;
+    }
+
+    return (globalThis.prisma = new PrismaClient());
+};
+
+export const prisma = createClient();
